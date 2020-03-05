6b8e41ae5007bd66351cc14b92e46c0fe19c994c
Bill Nicholson <nicholdw@ucmail.uc.edu>
2020-03-05 07:47:18 -0500

    First Commit

---
+++ OOPClassProject/Src/Main.py
@@ -0,0 +1,12 @@
+'''
+Created on Mar 5, 2020
+Class project to collaborate on a GitHub repo
+@author: nicomp
+'''
+from Team import Team
+bengals = Team("Bengals", "NFL Football")
+print(bengals.toString())
+
+steelers = Team("Steelers", "NFL Football")
+print(steelers.toString())
+  
---
+++ OOPClassProject/Src/Team.py
@@ -0,0 +1,13 @@
+'''
+Created on Mar 5, 2020
+Class project to collaborate on a GitHub repo
+@author: nicomp
+'''
+class Team:
+    def __init__(self, teamName, activity):
+        self.teamName = teamName
+        self.activity = activity
+        
+    def toString(self):
+        return "Team name = " + self.teamName + ", activity = " + self.activity 
+      
